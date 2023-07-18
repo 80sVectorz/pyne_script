1b6fb205d950386e3e4ebfcb68de08760e1d9376
80sVectorz <krijn@Vectrex>
2023-07-18 21:21:54 +0200

    Update

---
+++ examples/basics.py
@@ -0,0 +1,17 @@
+from pyne_script.series import Series
+
+series = Series(
+    key_value_pairs_int={"a": list(range(0, 6))},
+    track_history_mode=2,
+    initial_update=True,
+)
+# A should have value: past<-[0,1,2,3,4,5,.]->future
+#                                       ^ ^
+#                                       | |-After update the head would be here
+#                                       |- Head is currenty here. Update would finalize this value
+print(series["a"])  # should be 5
+print(series["a"][1])  # should be 4
+series["a"] = 6
+series.update()
+print(series["a"])  # should be 6
+print(series["a"][1])  # should be 5
