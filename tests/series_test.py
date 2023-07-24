7ce08d9d38af2a0014ac391f6b06f68fee706126
80sVectorz <krijn@Vectrex>
2023-07-24 20:57:51 +0200

    Updated tests to include multiple series.

--- tests/series_test.py
+++ tests/series_test.py
@@ -1,26 +1,26 @@
 from pyne_script.series import Series
 
-
 def test_series_short():
     for history_mode in [0,1,2]:
-        series = Series(
-            key_value_pairs_int={"a": list(range(0, 6))},
-            track_history_mode=history_mode,
-            initial_update=True,
-            window_size=10
-        )
-        print(history_mode)
+        for key in ["a","b"]:
+            series = Series(
+                key_value_pairs_int={"a": list(range(0, 6)),"b":list(range(0,6))},
+                track_history_mode=history_mode,
+                initial_update=True,
+                window_size=10
+            )
+            print("History_mode:",history_mode)
 
-        assert series.a == 5
-        assert series.a[1] == 4
+            assert series[key] == 5
+            assert series[key] == 4
 
-        series.a = 6
-        series.update()
-        assert series.a == 6
-        assert series.a[1] == 5
+            series[key] = 6
+            series.update()
+            assert series[key] == 6
+            assert series[key][1] == 5
 
-        assert list(series.a[:]) == [0,1,2,3,4,5,6]
-        assert list(series.a[3:]) == [4,5,6]
+            assert list(series[key][:]) == [0,1,2,3,4,5,6]
+            assert list(series[key][3:]) == [4,5,6]
 
 def test_series_long():
     for history_mode in [0,1,2]:
@@ -30,7 +30,8 @@ def test_series_long():
             initial_update=True,
             window_size=10
         )
-        print(history_mode)
+        print("History_mode:",history_mode)
+
 
         assert series.a == 19
         assert series.a[1] == 18
